06aba7c1ce116e14cd162a1f5d6423d57bfb5f79
Krossaj <edvin.strom.student12@smalandsgymnasiet.com>
2015-02-26 14:23:59 +0100

    Klassen collission

--- PingPong/PingPong/Collision.cs
+++ PingPong/PingPong/Collision.cs
@@ -3,9 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 namespace PingPong
 {
-    class Class1
+    class Collsion
     {
+        Texture2D texture;
+        public Vector2 Position;
+        public Vector2 Velocity;
+
+        public Rectangle Collision
+        {
+            get 
+            {
+                return new Rectangle(
+                    (int)Position.X,
+                    (int)Position.Y,
+                    texture.Width,
+                    texture.Height);
+            }
+        }
+
+        public Collsion(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.Position = position;
+        }
+
+        public Collsion(Texture2D texture, Vector2 position, Vector2 velocity)
+        {
+            this.texture = texture;
+            this.Position = position;
+            this.Velocity = velocity;
+        }
     }
 }
