022dcf3583af88d73eacfbec2fe52aecf28486ee
Jesper Lindström <hello@jesperlindstrom.me>
2015-02-26 14:55:37 +0100

    Work in progress på GameObject

---
+++ PingPong/PingPong/GameObjects/PlayerPaddle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PingPong.GameObjects
+{
+    class PlayerPaddle : GameObject
+    {
+        /// <summary>
+        /// Create a new player paddle at a specified position
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public PlayerPaddle(float x, float y) : base(x, y)
+        {
+
+        }
+    }
+}
