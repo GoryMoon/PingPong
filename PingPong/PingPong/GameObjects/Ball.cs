cd2591de9e173a3a5b3a7ed82db007c507d50717
Krossaj <edvin.strom.student12@smalandsgymnasiet.com>
2015-04-16 14:12:34 +0200

    boll
    något med bollen

--- PingPong/PingPong/GameObjects/Ball.cs
+++ PingPong/PingPong/GameObjects/Ball.cs
@@ -19,8 +19,12 @@ namespace PingPong.GameObjects
     public class Ball : GameObject
     {
         private Texture2D spriteTexture;
-        public int speed = 5;
+        public int speed = 1;
+        private int speedX;
+        private int speedY;
 
+        public int SpeedX { get {return speedX; } set { speedX = value; } }
+        public int SpeedY { get {return speedY; } set { speedY = value; } }
         /// <summary>
         /// Create a new player paddle at a specified position
         /// </summary>
@@ -28,7 +32,8 @@ namespace PingPong.GameObjects
         /// <param name="y"></param>
         public Ball(Game game, float x, float y) : base(game, x, y, 38, 38)
         {
-
+            speedX = speed;
+            speedY = speed;
         }
 
         /// <summary>
@@ -61,7 +66,19 @@ namespace PingPong.GameObjects
                 startOver = true;
             }
 
-            this.position.X += this.speed;
+            if (this.position.Y < 0)
+            {
+                this.speedY *= -1;   
+            }
+
+            else if (this.position.Y > 430)
+            {
+                this.speedY *= -1;
+            }
+
+            //this.position.X += this.speed;
+            this.position.X += this.speedX;
+            this.position.Y += this.speedY;
 
             if (startOver == true)
             {
@@ -81,6 +98,7 @@ namespace PingPong.GameObjects
             }
             this.Position.X = this.position.X;
             this.Position.Y = this.position.Y;
+
         }
 
         /// <summary>
